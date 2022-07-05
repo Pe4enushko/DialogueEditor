6c577af8ca5038c5020e762198662b598296ac4b
Александр Якимов <Mr.stone9999@gmail.com>
2022-07-05 21:15:56 +0400

    upload

---
+++ DialogueEditor/EditForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace DialogueEditor
+{
+    partial class EditForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_Cancel = new System.Windows.Forms.Button();
+            this.Btn_Ok = new System.Windows.Forms.Button();
+            this.Txt_Text = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Btn_Cancel
+            // 
+            this.Btn_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Btn_Cancel.Location = new System.Drawing.Point(438, 193);
+            this.Btn_Cancel.Name = "Btn_Cancel";
+            this.Btn_Cancel.Size = new System.Drawing.Size(102, 34);
+            this.Btn_Cancel.TabIndex = 0;
+            this.Btn_Cancel.Text = "Cancel";
+            this.Btn_Cancel.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Ok
+            // 
+            this.Btn_Ok.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.Btn_Ok.Location = new System.Drawing.Point(12, 193);
+            this.Btn_Ok.Name = "Btn_Ok";
+            this.Btn_Ok.Size = new System.Drawing.Size(102, 34);
+            this.Btn_Ok.TabIndex = 1;
+            this.Btn_Ok.Text = "OK";
+            this.Btn_Ok.UseVisualStyleBackColor = true;
+            // 
+            // Txt_Text
+            // 
+            this.Txt_Text.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Txt_Text.Location = new System.Drawing.Point(12, 13);
+            this.Txt_Text.Name = "Txt_Text";
+            this.Txt_Text.Size = new System.Drawing.Size(528, 159);
+            this.Txt_Text.TabIndex = 2;
+            this.Txt_Text.Text = "";
+            this.Txt_Text.TextChanged += new System.EventHandler(this.Txt_Text_TextChanged);
+            // 
+            // EditForm
+            // 
+            this.AcceptButton = this.Btn_Ok;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.Btn_Cancel;
+            this.ClientSize = new System.Drawing.Size(552, 239);
+            this.Controls.Add(this.Txt_Text);
+            this.Controls.Add(this.Btn_Ok);
+            this.Controls.Add(this.Btn_Cancel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "EditForm";
+            this.Text = "EditForm";
+            this.Load += new System.EventHandler(this.EditForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button Btn_Cancel;
+        private Button Btn_Ok;
+        private RichTextBox Txt_Text;
+    }
+}
---
+++ DialogueEditor/EditForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DialogueEditor
+{
+    public partial class EditForm : Form
+    {
+        public string EditedText { get; set; } = "";
+        public EditForm(string text)
+        {
+            InitializeComponent();
+            EditedText = text;
+        }
+
+        private void EditForm_Load(object sender, EventArgs e)
+        {
+            Txt_Text.Text = EditedText;
+        }
+
+        private void Txt_Text_TextChanged(object sender, EventArgs e)
+        {
+            EditedText = Txt_Text.Text;
+        }
+    }
+}
